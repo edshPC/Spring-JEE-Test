853d0fee564ad06a690afff36c65e7fbd9684193
edshPC <ed_shh@mail.ru>
2024-11-18 23:39:45 +0300

    OrderBy

--- JEE_test/src/main/java/com/is/jee_test/OrderRepository.java
+++ JEE_test/src/main/java/com/is/jee_test/OrderRepository.java
@@ -2,6 +2,7 @@ package com.is.jee_test;
 
 import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Find;
+import jakarta.data.repository.OrderBy;
 import jakarta.data.repository.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends BasicRepository<Order, Long> {
     @Find
+    @OrderBy(value = "date", descending = true)
     List<Order> findTopByUserOrderByDateDesc(User user);
 }
